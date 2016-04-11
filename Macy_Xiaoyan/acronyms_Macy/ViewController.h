b36ef69dff51df249ef6035ca59c9ffc816774f6
Shawn1248 <xiaoyan.zhao@hotmail.com>
2016-04-11 00:14:41 -0500

    Added files via upload

---
+++ Macy_Xiaoyan/acronyms_Macy/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.h
+//  acronyms_Macy
+//
+//  Created by ZHAOXIAOYAN on 4/8/16.
+//  Copyright © 2016 ShawnZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define ROOT_PATH @"http://www.nactem.ac.uk/software/acromine/dictionary.py?sf="
+
+@interface ViewController : UIViewController
+
+
+
+
+
+@end
